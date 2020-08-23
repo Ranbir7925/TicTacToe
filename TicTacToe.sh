d4b93a1a00c445a7be6c8e54755ae805996cddb4
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-23 14:17:42 +0530

    ADDED:Toss function added

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -14,4 +14,18 @@ function resettingBoard()
 	echo "|  ${board[7]}  ||  ${board[8]}  ||  ${board[9]}  |"
 	echo "**********************"
 }
- resettingBoard
+
+function tossToPlay()
+{
+	echo "Tossing coin....."
+	read -p "Press 1:HEADS or 2:TAILS: " choice
+	toss=$((RANDOM%2+1))
+	if (( $toss == $choice ))
+	then
+		echo "You won the toss You will play 1st"
+	else
+		echo "You lost the toss Computer will play 1st"
+	fi
+}
+resettingBoard
+tossToPlay
