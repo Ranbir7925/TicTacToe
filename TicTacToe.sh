eda2542b3a45d548c2730598ee57559d9ecd3341
Ranbir7925 <35680405+Ranbir7925@users.noreply.github.com>
2020-08-23 13:59:23 +0530

    ADDED:Reset board function added

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
 echo "Weclcome to the tic-tac-toe game"
+
+#array declaration of array
+declare -a board
+board=(. . . . . . . . . .)
+
+#Function to reset the board
+function resettingBoard()
+{
+	echo "***TicTacToe Board***"
+	echo "|  ${board[1]}  ||  ${board[2]}  ||  ${board[3]}  |"
+	echo "|  ${board[4]}  ||  ${board[5]}  ||  ${board[6]}  |"
+	echo "|  ${board[7]}  ||  ${board[8]}  ||  ${board[9]}  |"
+	echo "**********************"
+}
+ resettingBoard
